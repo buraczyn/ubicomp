b0e573d262ec4518870b0239da1b81d13f5aa964
Alex Buraczynski <alexb@allenai.org>
2020-04-25 15:55:46 -0700

    Modes done

--- A1NightLight/A1NightLight.ino
+++ A1NightLight/A1NightLight.ino
@@ -3,14 +3,9 @@ const int INPUT_MODE_2 = 3;
 const int INPUT_MODE_3 = 4;
 const int INPUT_MODE_4 = 5;
 const int OUTPUT_LED_PIN = LED_BUILTIN;
-
 const int DELAY_MS = 30;
 
-void readButton(int input) {
-  int buttonState = digitalRead(input);
-  digitalWrite(OUTPUT_LED_PIN, !buttonState);
-  Serial.println(buttonState);
-}
+int currentMode = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,13 +18,70 @@ void setup() {
   Serial.begin(9600);
 }
 
+int getCurrentMode() {
+  int mode1State = digitalRead(INPUT_MODE_1);
+  int mode2State = digitalRead(INPUT_MODE_2);
+  int mode3State = digitalRead(INPUT_MODE_3);
+  int mode4State = digitalRead(INPUT_MODE_4);
+
+  if (!mode1State == HIGH) {
+    return 1;
+  } else if (!mode2State == HIGH) {
+    return 2;
+  } else if (!mode3State == HIGH) {
+    return 3;
+  } else if (!mode4State == HIGH) {
+    return 4;
+  } else {
+    return currentMode;
+  }
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-  readButton(INPUT_MODE_1);
-  readButton(INPUT_MODE_2);
-  readButton(INPUT_MODE_3);
-  readButton(INPUT_MODE_4);
+  currentMode = getCurrentMode();
 
+  switch (currentMode) {
+    case 1:
+      mode1();
+      break;
+    case 2:
+      mode2();
+      break;
+    case 3:
+      mode3();
+      break;
+    case 4:
+      mode4();
+      break;
+    case 0: 
+      break;
+  }
+}
 
+// Crossfading RGB LED 
+void mode1() {
+  digitalWrite(OUTPUT_LED_PIN, HIGH);
+  Serial.println("Mode 1");
+}
 
+// Respond to sound
+void mode2() {
+  digitalWrite(OUTPUT_LED_PIN, HIGH);
+  Serial.println("Mode 2");
+
+}
+
+// Configuring colour with lofi input
+void mode3() {
+  digitalWrite(OUTPUT_LED_PIN, HIGH);
+  Serial.println("Mode 3");
+
+}
+
+// Creative feature
+void mode4() {
+  digitalWrite(OUTPUT_LED_PIN, HIGH);
+  Serial.println("Mode 4");
+  
 }
